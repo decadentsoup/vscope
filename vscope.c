af1d611822eafcdc49775c9d576ac2b1ef7e2f12
Megan Ruggiero <decadentsoup@users.noreply.github.com>
2019-01-21 03:10:09 -0500

    Update copyright years

--- vscope.c
+++ vscope.c
@@ -1,4 +1,4 @@
-// Copyright (C) 2018 Megan Ruggiero. All rights reserved.
+// Copyright (C) 2018-2019 Megan Ruggiero. All rights reserved.
 //
 // Permission to use, copy, modify, and distribute this software for any
 // purpose with or without fee is hereby granted, provided that the above
